6098972681693c162da99711b6607769dee443d1
sjs <seo777111@gmail.com>
2021-04-26 17:57:10 +0900

    chart.js used

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyChart from "./MyChart";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyChart />
   </React.StrictMode>,
   document.getElementById('root')
 );
